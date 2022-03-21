74f6f0239a37bd0b49b67437841f4b1ef6995046
vikaspandey <vikas@aidhere.de>
2022-03-21 15:23:58 +0530

    removed print statements

--- lib/repository/currency_pair_repository/currency_pair_repository.dart
+++ lib/repository/currency_pair_repository/currency_pair_repository.dart
@@ -8,26 +8,14 @@ class CurrencyPairRepository {
   Future<CurrencyPair> getCurrencyPair({
     required String currencyName,
   }) async {
-    print('Method called');
-
     String url = Constants.BASE_URL + Constants.tick + currencyName;
-
-    print('URL is: $url');
 
     final response = await http.get(Uri.parse(url));
 
-    print('response status code is: ${response.statusCode}');
-    print('response is: $response');
-
     if (response.statusCode == 200) {
-      CurrencyPair currencyPair =
-          CurrencyPair.fromJson(jsonDecode(response.body));
-
-      print('currencyPair: $currencyPair');
-
       return CurrencyPair.fromJson(jsonDecode(response.body));
     } else {
-      throw Exception('Failed to load album');
+      throw Exception('Failed to load data');
     }
   }
 }
