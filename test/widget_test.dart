567e71171c9767b6e1d554632d3e6de527df43b2
vikaspandey <vikas@aidhere.de>
2022-03-21 11:16:22 +0530

    Added widget test to find textfield and hint text on screen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,25 +1,85 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+import 'package:crypto_pair/repository/currency_pair_repository/currency_pair_repository.dart';
+import 'package:crypto_pair/repository/currency_pair_repository/model/currency_pair_model.dart';
+import 'package:crypto_pair/repository/order_book_repository/model/order_book_model.dart';
+import 'package:crypto_pair/repository/order_book_repository/order_book_repository.dart';
+import 'package:crypto_pair/screens/search/cubit/crypto_cubit.dart';
+import 'package:crypto_pair/screens/search/seatch_crypto.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 
-// void main() {
-//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//     // Build our app and trigger a frame.
-//     await tester.pumpWidget(const MyApp());
-//
-//     // Verify that our counter starts at 0.
-//     expect(find.text('0'), findsOneWidget);
-//     expect(find.text('1'), findsNothing);
-//
-//     // Tap the '+' icon and trigger a frame.
-//     await tester.tap(find.byIcon(Icons.add));
-//     await tester.pump();
-//
-//     // Verify that our counter has incremented.
-//     expect(find.text('0'), findsNothing);
-//     expect(find.text('1'), findsOneWidget);
-//   });
-// }
+class MockCurrencyPairRepository extends Mock
+    implements CurrencyPairRepository {}
+
+class MockOrderBookRepository extends Mock implements OrderBookRepository {}
+
+void main() {
+  late MockCurrencyPairRepository mockCurrencyPairRepository;
+  late MockOrderBookRepository mockOrderBookRepository;
+
+  var cryptoPair = CurrencyPair(
+    high: '38305.24',
+    low: '38305.24',
+    open: '38305.24',
+    last: '38305.24',
+    volume: '38305.24',
+    timestamp: DateTime.now(),
+  );
+
+  var orderBook = const OrderBook(bids: [
+    ["41960.39", "0.14852219"],
+    ["41960.39", "0.14852219"],
+    ["41960.39", "0.14852219"],
+    ["41960.39", "0.14852219"],
+    ["41960.39", "0.14852219"],
+  ], asks: [
+    ["41960.39", "0.14852219"],
+    ["41960.39", "0.14852219"],
+    ["41960.39", "0.14852219"],
+    ["41960.39", "0.14852219"],
+    ["41960.39", "0.14852219"],
+  ]);
+
+  setUp(() {
+    mockCurrencyPairRepository = MockCurrencyPairRepository();
+    mockOrderBookRepository = MockOrderBookRepository();
+  });
+
+  void getCurrencyAndOrderBookData() {
+    when(() =>
+            mockCurrencyPairRepository.getCurrencyPair(currencyName: 'btcusd'))
+        .thenAnswer((_) async => cryptoPair);
+
+    when(() => mockOrderBookRepository.getOrderBookData(currencyName: 'btcusd'))
+        .thenAnswer((_) async => orderBook);
+  }
+
+  Widget createTestWidget() {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: BlocProvider(
+        create: (_) => CryptoCubit(
+          currencyPairRepository: mockCurrencyPairRepository,
+          orderBookRepository: mockOrderBookRepository,
+        ),
+        child: const SearchCryptoScreen(),
+      ),
+    );
+  }
+
+  testWidgets('search text box should be displayed',
+      (WidgetTester tester) async {
+    //getCurrencyAndOrderBookData();
+    await tester.pumpWidget(createTestWidget());
+    expect(find.byType(TextFormField), findsOneWidget);
+    expect(find.text('Enter currency pair'), findsOneWidget);
+  });
+
+  testWidgets(
+      'loading indicators should be displayed while loading crypto data',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(createTestWidget());
+  });
+}
